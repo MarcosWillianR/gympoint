718287a5940c8da6e9aa9c2a3504a338a6031693
Marcos Willian <markusuuuu@gmail.com>
2019-11-30 23:34:47 -0300

    feature/ created middleware for authentication

---
+++ backend/src/app/middlewares/auth.js
@@ -0,0 +1,22 @@
+import jwt from 'jsonwebtoken';
+import { promisify } from 'util';
+
+import authConfig from '../../configs/Auth';
+
+export default async (req, res, next) => {
+  const bearerToken = req.headers.authorization;
+
+  if (!bearerToken) return res.status(401).json({ error: 'Token not provided' });
+
+  const [, token] = bearerToken.split(' ');
+
+  try {
+    const decoded = await promisify(jwt.verify)(token, authConfig.secretKey);
+
+    req.user = decoded.id;
+
+    return next();
+  } catch (err) {
+    return res.status(401).json({ error: 'Token invalid' });
+  }
+}
