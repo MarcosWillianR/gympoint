e98c22409a15b26a2ed05843aab2312515e35c49
Marcos Willian <markusuuuu@gmail.com>
2019-12-01 01:16:40 -0300

    improvement/ created validations for controllers

--- backend/src/app/controllers/SessionController.js
+++ backend/src/app/controllers/SessionController.js
@@ -1,4 +1,5 @@
 import jwt from 'jsonwebtoken';
+import * as Yup from 'yup';
 
 import User from '../models/User';
 
@@ -6,6 +7,15 @@ import AuthConfig from '../../configs/Auth';
 
 class SessionController {
   async store(req, res) {
+    const schema = Yup.object().shape({
+      email: Yup.string().email().required(),
+      password: Yup.string().min(6).required(),
+    });
+
+    if(!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation fails' });
+    }
+
     const user = await User.findOne({ where: { email: req.body.email } });
 
     if (!user) return res.status(401).json({ error: 'User not found' });
