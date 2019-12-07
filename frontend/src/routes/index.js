0773ec7fb2c402e6388cd231a560ff1b969c810d
Marcos Willian <markusuuuu@gmail.com>
2019-12-07 20:41:06 -0300

    created component for private routes

--- frontend/src/routes/index.js
+++ frontend/src/routes/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Switch } from 'react-router-dom';
+import Route from './Route';
 
 import SignIn from '../pages/SignIn';
 
@@ -9,8 +10,10 @@ export default function Routes() {
   return (
     <Switch>
       <Route path="/" exact component={SignIn} />
-      
-      <Route path="dashboard" component={Dashboard} />
+
+      <Route path="/dashboard" component={Dashboard} isPrivate />
+
+      {/* }<Route path="/" component={() => <h1>404</h1>} />{ */}
     </Switch>
-  )
+  );
 }
