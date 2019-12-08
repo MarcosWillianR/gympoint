62b1b8fbb79875c5b7031b61dc0f7e2e60dd4d73
Marcos Willian <markusuuuu@gmail.com>
2019-12-08 01:37:19 -0300

    created style for SignIn page

--- frontend/src/pages/SignIn/index.js
+++ frontend/src/pages/SignIn/index.js
@@ -1,7 +1,35 @@
 import React from 'react';
+import { Form, Input } from '@rocketseat/unform';
 
-// import { Container } from './styles';
+import logo from '~/assets/images/logo.png';
 
 export default function SignIn() {
-  return <h1>SIGN IN</h1>;
+  const handleSubmit = data => {
+    console.tron.log(data);
+  };
+
+  return (
+    <>
+      <img src={logo} alt="Gympoint" />
+
+      <Form onSubmit={handleSubmit}>
+        <label htmlFor="email">Seu e-mail</label>
+        <Input
+          name="email"
+          id="email"
+          type="email"
+          placeholder="exemplo@email.com"
+        />
+        <label htmlFor="senha">Sua senha</label>
+        <Input
+          name="senha"
+          id="senha"
+          type="password"
+          placeholder="*************"
+        />
+
+        <button type="submit">Entrar no sistema</button>
+      </Form>
+    </>
+  );
 }
