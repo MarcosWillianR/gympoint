ea8551bf3ec166d0282db835ba1a341229008614
Marcos Willian <markusuuuu@gmail.com>
2019-12-09 02:18:09 -0300

    created helper components and SignIn page style done

---
+++ mobile/src/components/Input/styles.js
@@ -0,0 +1,18 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.View`
+  padding: 0 15px;
+  height: 50px;
+  border: 1px solid rgba(0, 0, 0, 0.1);
+  border-radius: 4px;
+  flex-direction: row;
+  align-items: center;
+`;
+
+export const TInput = styled.TextInput.attrs({
+  placeholderTextColor: '#999',
+})`
+  flex: 1;
+  font-size: 16px;
+  color: #444;
+`;
--- mobile/src/pages/SignIn/index.js
+++ mobile/src/pages/SignIn/index.js
@@ -1,8 +1,28 @@
-import React from 'react';
-import { Text } from 'react-native';
+import React, { useRef } from 'react';
+import { Image } from 'react-native';
 
-// import { Container } from './styles';
+import logo from '~/assets/images/logo.png';
+
+import { Container, Form, FormInput, SubmitButton } from './styles';
 
 export default function SignIn() {
-  return <Text>SignIn</Text>;
+  const studentIDRef = useRef();
+
+  function handleSubmit() {}
+  return (
+    <Container>
+      <Image source={logo} />
+
+      <Form>
+        <FormInput
+          placeholder="Informe seu ID de cadastro"
+          ref={studentIDRef}
+          returnKeyType="send"
+          onSubmitEditing={handleSubmit}
+        />
+
+        <SubmitButton onPress={handleSubmit}>Entrar no sistema</SubmitButton>
+      </Form>
+    </Container>
+  );
 }
