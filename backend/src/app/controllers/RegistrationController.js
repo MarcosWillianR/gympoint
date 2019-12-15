a5246c323a30ab8866ca8e55d943bb4917ce177c
Marcos Willian <markusuuuu@gmail.com>
2019-12-15 03:13:33 -0300

    created store method of registration service

--- backend/src/app/controllers/RegistrationController.js
+++ backend/src/app/controllers/RegistrationController.js
@@ -1,7 +1,9 @@
 import * as Yup from 'yup';
+import { addMonths, parseISO } from 'date-fns';
 
 import Registration from '../models/Registration';
-
+import Plan from '../models/Plan';
+import Student from '../models/Student';
 
 class RegistrationController {
   async index(req, res) {
@@ -11,7 +13,69 @@ class RegistrationController {
   }
 
   async store(req, res) {
-    return res.json({ message: 'ok' });
+    const schema = Yup.object().shape({
+      student_id: Yup.number().required(),
+      plan_id: Yup.number().required(),
+      start_date: Yup.date().required(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation fails' });
+    }
+
+    const { student_id, plan_id, start_date } = req.body;
+
+    /**
+     * Verifica se o aluno não existe
+     */
+    const studentExists = await Student.findByPk(student_id);
+
+    if (!studentExists) {
+      return res.status(400).json({ error: 'Student does not exists' });
+    }
+    /**
+     * Verifica se o plano existe
+     */
+    const planChoosed = await Plan.findByPk(plan_id);
+
+    if (!planChoosed) {
+      return res.status(400).json({ error: 'Plan dont exists' });
+    }
+
+    const { duration, price } = planChoosed;
+
+    /**
+     * Verifica se já possuí matricula
+     */
+    const alreadyRegistered = await Registration.findOne({
+      where: {
+        student_id,
+      },
+    });
+
+    if (alreadyRegistered) {
+      return res.status(400).json({ error: 'User already registered' });
+    }
+
+    /**
+     * Regra de negócio para informar a data final com base no plano escolhido
+     */
+    const end_date = addMonths(parseISO(start_date), duration);
+
+    /**
+     * Regra de negócio para somar o valor final da matrícula
+     */
+    const total_price = price * duration;
+
+    await Registration.create({
+      student_id,
+      plan_id,
+      start_date,
+      end_date,
+      price: total_price,
+    });
+
+    return res.json({ message: 'Registration was created' });
   }
 
   async update(req, res) {
--- backend/src/app/models/Registration.js
+++ backend/src/app/models/Registration.js
@@ -2,11 +2,14 @@ import { Model, DataTypes } from 'sequelize';
 
 class Registration extends Model {
   static init(sequelize) {
-    super.init({
-      start_date: DataTypes.DATE,
-      end_date: DataTypes.DATE,
-      price: DataTypes.DECIMAL(10, 2),
-    }, { sequelize });
+    super.init(
+      {
+        start_date: DataTypes.DATE,
+        end_date: DataTypes.DATE,
+        price: DataTypes.DECIMAL(10, 2),
+      },
+      { sequelize }
+    );
 
     return this;
   }
