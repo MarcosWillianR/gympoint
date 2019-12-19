c308b71c2f46592982c6b78bdb1386a0e7e97c26
Marcos Willian <markusuuuu@gmail.com>
2019-12-18 23:18:19 -0300

    mobile session controller and route created

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -4,6 +4,7 @@ import SessionController from './app/controllers/SessionController';
 import StudentController from './app/controllers/StudentController';
 import PlanController from './app/controllers/PlanController';
 import RegistrationController from './app/controllers/RegistrationController';
+import MobileSessionController from './app/controllers/MobileSessionController';
 
 import authMiddleware from './app/middlewares/auth';
 
@@ -11,6 +12,11 @@ const routes = new Router();
 
 routes.post('/sessions', SessionController.store);
 
+routes.post('/mobile-sessions', MobileSessionController.store);
+
+/**
+ * Rotas FRONT WEB
+ */
 routes.use(authMiddleware);
 
 routes.get('/students', StudentController.index);
@@ -25,5 +31,9 @@ routes.get('/registrations', RegistrationController.index);
 routes.post('/registrations', RegistrationController.store);
 routes.put('/registrations/:reg_id', RegistrationController.update);
 routes.delete('/registrations/:reg_id', RegistrationController.delete);
+
+/**
+ * Rotas FRONT MOBILE
+ */
 
 export default routes;
