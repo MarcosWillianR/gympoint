7c30010c37c35464523f5384e8a07efbbc7b192a
Marcos Willian <markusuuuu@gmail.com>
2019-12-21 19:14:20 -0300

    redux, redux-saga and redux-persist with AsyncStorage of react-native configuredsuccessfuly

--- mobile/src/index.js
+++ mobile/src/index.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import { StatusBar } from 'react-native';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+
 import '~/config/ReactotronConfig';
 
 import Routes from '~/routes';
 
+import { persistor, store } from '~/store';
+
 export default function App() {
   return (
-    <>
-      <StatusBar barStyle="dark-content" backgroundColor="#fff" />
-      <Routes />
-    </>
+    <Provider store={store}>
+      <PersistGate persistor={persistor}>
+        <StatusBar barStyle="dark-content" backgroundColor="#fff" />
+        <Routes />
+      </PersistGate>
+    </Provider>
   );
 }
