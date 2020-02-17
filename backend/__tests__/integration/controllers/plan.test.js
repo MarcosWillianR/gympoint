70a28424dd4c9953c28984a8ccf72065be5857cd
Marcos Willian <markusuuuu@gmail.com>
2020-02-16 23:11:52 -0300

    created test for creation

--- backend/__tests__/integration/controllers/plan.test.js
+++ backend/__tests__/integration/controllers/plan.test.js
@@ -2,16 +2,33 @@ import request from 'supertest';
 import app from '../../../src/app';
 
 import factory from '../../factories';
+
+// Utils
 import truncate from '../../utils/truncate';
+import auth from '../../utils/authentication';
+
+// Bearer Token
+let adminToken;
 
 describe('Plan', () => {
   beforeEach(async () => {
     await truncate();
   });
 
-  it('should be able to create a new Plan using Sequelize', async () => {
-    const plan = await factory.create('Plan');
+  beforeAll(async () => {
+    const token = await auth();
 
-    expect(plan).toHaveProperty('id');
+    adminToken = `Bearer ${token}`;
+  });
+
+  it('should be able to create a new Plan', async () => {
+    const planAttributes = await factory.attrs('Plan');
+
+    const newPlan = await request(app)
+      .post('/plans')
+      .set('Authorization', adminToken)
+      .send(planAttributes);
+
+    expect(newPlan.body).toHaveProperty('id');
   });
 });
