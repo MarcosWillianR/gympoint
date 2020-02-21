ec1e1df256e2494a0321703ea6a367a4b16a9de1
Marcos Willian <markusuuuu@gmail.com>
2020-02-21 02:15:15 -0300

    teste para editar um plano que não existe e deletar um plano que está em uso

--- backend/__tests__/integration/controllers/plan.test.js
+++ backend/__tests__/integration/controllers/plan.test.js
@@ -1,4 +1,5 @@
 import request from 'supertest';
+import { addMonths, parseISO, format } from 'date-fns';
 import app from '../../../src/app';
 
 import factory from '../../factories';
@@ -6,6 +7,9 @@ import factory from '../../factories';
 // Utils
 import truncate from '../../utils/truncate';
 import auth from '../../utils/authentication';
+
+// models
+import Registration from '../../../src/app/models/Registration';
 
 // Bearer Token
 let adminToken;
@@ -94,7 +98,7 @@ describe('Plan', () => {
     expect(plans.body).toHaveLength(10);
   });
 
-  it('find an non-existent plan', async () => {
+  it('find a non-existent plan', async () => {
     await factory.createMany('Plan', 10);
 
     const plans = await request(app)
@@ -103,5 +107,54 @@ describe('Plan', () => {
       .expect(400);
 
     expect(plans.body.error).toBe('Esse plano não existe.');
+  });
+
+  it('edit a non-existent plan', async () => {
+    await factory.createMany('Plan', 10);
+
+    const plans = await request(app)
+      .put('/plans/11')
+      .set('Authorization', adminToken)
+      .expect(400);
+
+    expect(plans.body.error).toBe('Esse plano não existe.');
+  });
+
+  it('delete a non-existent plan', async () => {
+    await factory.createMany('Plan', 10);
+
+    const plans = await request(app)
+      .delete('/plans/11')
+      .set('Authorization', adminToken)
+      .expect(400);
+
+    expect(plans.body.error).toBe('Esse plano não existe.');
+  });
+
+  it('Error when trying to delete plan in use', async () => {
+    const { id: student_id } = await factory.create('Student');
+    const { id: plan_id, duration, price } = await factory.create('Plan');
+    const start_date = new Date();
+
+    const end_date = addMonths(parseISO(start_date), duration);
+    const total_price = price * duration;
+
+    const { plan_id: registration_plan_id } = await Registration.create({
+      student_id,
+      plan_id,
+      start_date,
+      end_date,
+      price: total_price,
+    });
+
+    const deletedPlanInUse = await request(app)
+      .delete(`/plans/${registration_plan_id}`)
+      .set('Authorization', adminToken)
+      .expect(400);
+
+    expect(deletedPlanInUse.body).toHaveProperty(
+      'error',
+      'Esse plano está sendo utilizado.'
+    );
   });
 });
