536ed74079b99c0972f3e766cee99992d073b0bf
Marcos Willian <markusuuuu@gmail.com>
2020-02-29 16:12:26 -0300

    abstraindo mensagens de retono ao front

---
+++ backend/src/utils/validations/index.js
@@ -0,0 +1,3 @@
+import pt_br from './messages/pt_br';
+
+export { pt_br };
---
+++ backend/src/utils/validations/messages/pt_br.js
@@ -0,0 +1,7 @@
+export default {
+  plans: {
+    not_exists: 'Esse plano não existe.',
+    being_used: 'Esse plano está sendo utilizado.',
+    success_removed: 'Plano removido com sucesso!',
+  },
+};
