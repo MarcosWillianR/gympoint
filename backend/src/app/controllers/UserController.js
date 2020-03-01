7025b6fdbff888c125c06c3b03a8851ef382adfc
Marcos Willian <markusuuuu@gmail.com>
2020-03-01 14:36:21 -0300

    refatorando controller

--- backend/src/app/controllers/UserController.js
+++ backend/src/app/controllers/UserController.js
@@ -1,20 +1,7 @@
-import * as Yup from 'yup';
 import User from '../models/User';
 
 class UserController {
   async store(req, res) {
-    const schema = Yup.object().shape({
-      name: Yup.string(),
-      email: Yup.string()
-        .email()
-        .required(),
-      password: Yup.string().required(),
-    });
-
-    if (!(await schema.isValid(req.body))) {
-      return res.status(400).json({ error: 'Validation fails' });
-    }
-
     const newAdminUser = await User.create(req.body);
 
     return res.status(201).json(newAdminUser);
