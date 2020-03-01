c112251bd9c985083d943f7833c6019586ff3d1f
Marcos Willian <markusuuuu@gmail.com>
2020-03-01 17:28:07 -0300

    abstraindo validator de mobile session

---
+++ backend/src/app/validators/MobileSessionStore.js
@@ -0,0 +1,17 @@
+import * as Yup from 'yup';
+
+export default async (req, res, next) => {
+  try {
+    const schema = Yup.object().shape({
+      student_id: Yup.number().required(),
+    });
+
+    await schema.validate(req.body, { abortEarly: false });
+
+    return next();
+  } catch (err) {
+    return res
+      .status(400)
+      .json({ error: 'Erro de validação', messages: err.inner });
+  }
+};
