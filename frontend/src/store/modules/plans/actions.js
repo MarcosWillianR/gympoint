572a358d80203743534bbac7f8247347d25b2728
Marcos Willian <markusuuuu@gmail.com>
2020-03-02 01:35:58 -0300

    recebendo mensagens da resposta do servidor na rota de planos

--- frontend/src/store/modules/plans/actions.js
+++ frontend/src/store/modules/plans/actions.js
@@ -1,32 +1,32 @@
 export function plansSuccess() {
   return {
-    type: '@plans/PLANS_SUCCESS',
+    type: '@plans/REQUEST_SUCCESS',
   };
 }
 
 export function plansFailure() {
   return {
-    type: '@plans/PLANS_FAILURE',
+    type: '@plans/REQUEST_FAILURE',
   };
 }
 
 export function plansCreateRequest(title, duration, price) {
   return {
-    type: '@plans/PLANS_CREATE_REQUEST',
+    type: '@plans/CREATE_REQUEST',
     payload: { title, duration, price },
   };
 }
 
 export function plansEditRequest(id, title, duration, price) {
   return {
-    type: '@plans/PLANS_EDIT_REQUEST',
+    type: '@plans/EDIT_REQUEST',
     payload: { id, title, duration, price },
   };
 }
 
 export function plansDeleteRequest(id) {
   return {
-    type: '@plans/PLANS_DELETE_REQUEST',
+    type: '@plans/DELETE_REQUEST',
     payload: { id },
   };
 }
